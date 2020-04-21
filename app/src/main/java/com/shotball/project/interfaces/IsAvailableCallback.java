01049dc74ab384b810f50262ad21dc222bf13341
Danil <danilvda@gmail.com>
2020-04-21 21:42:42 +0300

    Added exchanges

---
+++ app/src/main/java/com/shotball/project/interfaces/IsAvailableCallback.java
@@ -0,0 +1,5 @@
+package com.shotball.project.interfaces;
+
+public class DatabaseListenerInterface {
+    
+}
---
+++ app/src/main/java/com/shotball/project/models/ExchangeModel.java
@@ -0,0 +1,4 @@
+package com.shotball.project.models;
+
+public class ExchangeModel {
+}
